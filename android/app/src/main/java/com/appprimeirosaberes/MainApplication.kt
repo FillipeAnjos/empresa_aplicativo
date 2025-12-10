022981bf0a4fd0901c3e9660bb23318d7d767d0e
Fillipe Anjos <fillipe.anjos.2000@hotmail.com>
2025-12-10 08:35:42 -0300

    atualizacao ...

--- android/app/src/main/java/com/appprimeirosaberes/MainApplication.kt
+++ android/app/src/main/java/com/appprimeirosaberes/MainApplication.kt
@@ -9,7 +9,6 @@ import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
-import android.webkit.WebView
 
 class MainApplication : Application(), ReactApplication {
 
@@ -35,6 +34,5 @@ class MainApplication : Application(), ReactApplication {
   override fun onCreate() {
     super.onCreate()
     loadReactNative(this)
-    WebView.setWebContentsDebuggingEnabled(true)
   }
 }
